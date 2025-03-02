605952338abcb7e5a9df95b065d1891281259b3b
Tech1k <hello@tech1k.com>
2025-03-02 01:20:51 -0700

    Update Litecoin chainparams

--- app/coins/ltc.js
+++ app/coins/ltc.js
@@ -3,8 +3,6 @@
 const Decimal = require("decimal.js");
 const Decimal8 = Decimal.clone({ precision:8, rounding:8 });
 
-const ltcFun = require("./ltcFun.js");
-
 const blockRewardEras = [ new Decimal8(50) ];
 for (let i = 1; i < 34; i++) {
 	let previous = blockRewardEras[i - 1];
@@ -102,9 +100,9 @@ module.exports = {
 	minTxWeight: 166 * 4, // hack
 	difficultyAdjustmentBlockCount: 2016,
 	maxSupplyByNetwork: {
-		"main": new Decimal(20999817.31308491), // ref: https://bitcoin.stackexchange.com/a/38998
-		"test": new Decimal(21000000),
-		"regtest": new Decimal(21000000)
+		"main": new Decimal(84000000),
+		"test": new Decimal(84000000),
+		"regtest": new Decimal(84000000)
 	},
 	targetBlockTimeSeconds: 600,
 	targetBlockTimeMinutes: 10,
@@ -128,8 +126,8 @@ module.exports = {
 
 	// used for supply estimates that don't need full gettxoutset accuracy
 	coinSupplyCheckpointsByNetwork: {
-		"main": [ 675046, new Decimal(18656332.38) ],
-		"test": [ 1940614, new Decimal(20963051.112) ],
+		"main": [ 2854525, new Decimal(75588768.58) ],
+		"test": [ 1940614, new Decimal(20963051.112) ], // TODO: update for Litecoin testnet
 		"regtest": [ 0, new Decimal(0) ]
 	},
 
@@ -137,45 +135,45 @@ module.exports = {
 		// this includes values from running gettxoutsetinfo with both "muhash" and "hash_serialized_2" params
 		"main": {
 			// "muhash"
-			"height": 784796,
-			"bestblock": "000000000000000000026ac332dc8ba0d425b844520acc808af88aac52748281",
-			"txouts": 87769791,
-			"bogosize": 6562752541,
-			"muhash": "90dc87ec3d3dc46a9883f1ce675cc8bcc3e6697c6c1dbb8665c192af35144099",
-			"total_amount": "19342261.957857",
-			"total_unspendable_amount": "219.292143",
+			"height": 2854525,
+			"bestblock": "bf5210ea43af6dd2f3aa2c69620c6326554280d2edf4f67da6ca1b29c00349b0",
+			"txouts": 57934228,
+			"bogosize": 4268119225,
+			"muhash": "b898636ba60c053ea5961146436307264f7bbfc43ee3931b6b69acf2c4154f71",
+			"total_amount": "75588768.58557639",
+			"total_unspendable_amount": "50.00", // coinbase transaction
 
 			// "hash_serialized_2"
-			"transactions": 52250541,
-			"disk_size": 5367051020,
-			"hash_serialized_2": "89afe21688f3a2cc01ef837c2b0454d4039830433d49c264856b2578eff2d62b",
+			"transactions": 32768281,
+			"disk_size": 3477711444,
+			"hash_serialized_2": "b1c37dd81bfa0b40d9858366a32b17e4a6f71ae03fe3b76579cb82a57b2441d1",
 
-			"lastUpdated": 1681140656788
+			"lastUpdated": 1740926448
 		}
 	},
 	
 	genesisBlockHashesByNetwork:{
-		"main":	"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-		"test":	"000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943",
-		"regtest": "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206",
+		"main":	"12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
+		"test":	"4966625a4b2851d9fdee139e56211a0d88575f59ed816ff5e6a63deb4e3e29a0",
+		"regtest": "530827f38f93b43ed12af0b3ad25a288dc02ed74d6d7857862df51fc56c416f9",
 	},
 	genesisCoinbaseTransactionIdsByNetwork: {
-		"main":	"4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+		"main":	"12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
 		"test":	"4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
 		"regtest": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
 	},
 	genesisCoinbaseTransactionsByNetwork:{
 		"main": {
-			"hex": "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0804ffff001d02fd04ffffffff0100f2052a01000000434104f5eeb2b10c944c6b9fbcfff94c35bdeecd93df977882babc7f3a2cf7f5c81d3b09a68db7f0e04f21de5d4230e75e6dbe7ad16eefe0d4325a62067dc6f369446aac00000000",
-			"txid": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
-			"hash": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
-			"size": 204,
-			"vsize": 204,
+			"hex": "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4804ffff001d0104404e592054696d65732030352f4f63742f32303131205374657665204a6f62732c204170706c65e280997320566973696f6e6172792c2044696573206174203536ffffffff0100f2052a010000004341040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9ac00000000",
+			"txid": "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9",
+			"hash": "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9",
+			"size": 280,
+			"vsize": 280,
 			"version": 1,
-			"confirmations":475000,
+			"confirmations":2854531,
 			"vin": [
 				{
-					"coinbase": "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73",
+					"coinbase": "04ffff001d0104404e592054696d65732030352f4f63742f32303131205374657665204a6f62732c204170706c65e280997320566973696f6e6172792c2044696573206174203536",
 					"sequence": 4294967295
 				}
 			],
@@ -184,8 +182,8 @@ module.exports = {
 					"value": 50,
 					"n": 0,
 					"scriptPubKey": {
-						"asm": "04f5eeb2b10c944c6b9fbcfff94c35bdeecd93df977882babc7f3a2cf7f5c81d3b09a68db7f0e04f21de5d4230e75e6dbe7ad16eefe0d4325a62067dc6f369446a OP_CHECKSIG",
-						"hex": "4104f5eeb2b10c944c6b9fbcfff94c35bdeecd93df977882babc7f3a2cf7f5c81d3b09a68db7f0e04f21de5d4230e75e6dbe7ad16eefe0d4325a62067dc6f369446aac",
+						"asm": "040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9 OP_CHECKSIG",
+						"hex": "41040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9ac",
 						"reqSigs": 1,
 						"type": "pubkey",
 						"addresses": [
@@ -194,9 +192,9 @@ module.exports = {
 					}
 				}
 			],
-			"blockhash": "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-			"time": 1230988505,
-			"blocktime": 1230988505
+			"blockhash": "12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
+			"time": 1317972665,
+			"blocktime": 1317972665
 		},
 		"test": {
 			"hex": "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000",
@@ -268,7 +266,7 @@ module.exports = {
 			"avgfee": 0,
 			"avgfeerate": 0,
 			"avgtxsize": 0,
-			"blockhash": "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
+			"blockhash": "12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
 			"feerate_percentiles": [
 				0,
 				0,
@@ -282,7 +280,7 @@ module.exports = {
 			"maxfeerate": 0,
 			"maxtxsize": 0,
 			"medianfee": 0,
-			"mediantime": 1231006505,
+			"mediantime": 1317972665,
 			"mediantxsize": 0,
 			"minfee": 0,
 			"minfeerate": 0,
@@ -292,14 +290,14 @@ module.exports = {
 			"swtotal_size": 0,
 			"swtotal_weight": 0,
 			"swtxs": 0,
-			"time": 1231006505,
+			"time": 1317972665,
 			"total_out": 0,
 			"total_size": 0,
 			"total_weight": 0,
 			"totalfee": 0,
 			"txs": 1,
 			"utxo_increase": 1,
-			"utxo_size_inc": 117
+			"utxo_size_inc": 280
 		},
 		"test": {
 			"avgfee": 0,
@@ -378,73 +376,24 @@ module.exports = {
 	},
 	testData: {
 		txDisplayTestList: {
-			"634b57cf0673c50b98560dbdf48d0a8633303b5d9162175e08b304df159c259e" : {
-				blockHeight: 694670, blockHash: "0000000000000000000ba61d43854a2460b219b5281db2c731ae03a4347eaf43"
+			"4508218dffb92d56d0a95cdb6fb1aa0dd0f2f20cc9a6016e6dc0b5657a6dd66b" : {
+				blockHeight: 383640, blockHash: "2b6809f094a9215bafc65eb3f110a35127a34be94b7d0590a096c3f126c6f364"
 			},
-			"f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16" : {
-				blockHeight: 170, blockHash: "00000000d1145790a8694403d4063f323d499e655c83426834d4ce2f8dd4a2ee"
+			"cd5239f7280643c9e7e75223fe3f7bc067787981b2462a16c148064370f1702c" : {
+				blockHeight: 409004, blockHash: "487518d663d9f1fa08611d9395ad74d982b667fbdc0e77e9cf39b4f1355908a3"
 			},
-			"a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d" : {
-				blockHeight: 57043, blockHash: "00000000152340ca42227603908689183edc47355204e7aca59383b0aaac1fd8"
+			"72e893f6c0e9a65ec227587b93aa45f21ab2f2a546b979654cc6443309e8a75b" : {
+				blockHeight: 456000, blockHash: "bf34f71cc6366cd487930d06be22f897e34ca6a40501ac7d401be32456372004"
 			},
-			"7b6e490670a5cfcc9b66d8aab142ac2e9b489ae7f40cadadfc69c19878ae81b0" : {
-				blockHeight: 227835, blockHash: "00000000000001aa077d7aa84c532a4d69bdbff519609d1da0835261b7a74eb6"
+			"fdb1ad4363a926558287927652a47cde1dba0bb40eda784b705e75d3043ea92e" : {
+				blockHeight: 638902, blockHash: "15238656e8ec63d28de29a8c75fcf3a5819afc953dcd9cc45cecc53baec74f38"
 			},
-			"8f7f13d6b56ea9013f13d298bc0e9e9f4f9825f3e7fd96083a564b10b01025d9" : {
-				blockHeight: 694521, blockHash: "00000000000000000009974b5f6011d7ec8af460dafcc668c7ede4324896b9ca"
-			},
-			"3215f4a32a26938ddf9eeb4de7f5f42e751410876500f6e93d943abb2c3cccc4" : {
-				blockHeight: 694521, blockHash: "00000000000000000009974b5f6011d7ec8af460dafcc668c7ede4324896b9ca"
-			},
-			"333d5f27c6fc2d07ef8c19e17d33568706bc3d6875198aba6cff0a996698d46e" : {
-				blockHeight: 694521, blockHash: "00000000000000000009974b5f6011d7ec8af460dafcc668c7ede4324896b9ca"
-			},
-			"a9ceb47b092f703c30b29cb8b864fb8fa895a5999b24aa56ae08a967b643087c" : {
-				blockHeight: 694521, blockHash: "00000000000000000009974b5f6011d7ec8af460dafcc668c7ede4324896b9ca"
-			},
-			"bc968c93c6ff39f022f974504a22d548902fe5a8c4fb294f052f845e4c388fcb" : {
-				blockHeight: 694521, blockHash: "00000000000000000009974b5f6011d7ec8af460dafcc668c7ede4324896b9ca"
-			},
-			"e4bd7949cbf067d17629a5f588bba051b4436d29b5978d674118539356745bd0" : {
-				blockHeight: 227835, blockHash: "00000000000001aa077d7aa84c532a4d69bdbff519609d1da0835261b7a74eb6"
-			},
-			"54e48e5f5c656b26c3bca14a8c95aa583d07ebe84dde3b7dd4a78f4e4186e713" : {
-				blockHeight: 230009, blockHash: "00000000000000ecbbff6bafb7efa2f7df05b227d5c73dca8f2635af32a2e949"
-			},
-			"d29c9c0e8e4d2a9790922af73f0b8d51f0bd4bb19940d9cf910ead8fbe85bc9b" : {
-				blockHeight: 268060, blockHash: "000000000000000743aee48cf264e1aa4a05fc3018677be3c1bdbd2429ffeede"
-			},
-			"143a3d7e7599557f9d63e7f224f34d33e9251b2c23c38f95631b3a54de53f024" : {
-				blockHeight: 306204, blockHash: "000000000000000038dea6f503ed3593b1495e135d9ed646c2ebb97a1ff35bd7"
-			},
-			"8f907925d2ebe48765103e6845c06f1f2bb77c6adc1cc002865865eb5cfd5c1c" : {
-				blockHeight: 481824, blockHash: "0000000000000000001c8018d9cb3b742ef25114f27563e3fc4a1902167f9893"
-			},
-			"8f5834d39a634c1b4c6283b546e16e931cb34d28570c77860de1a86256c4344d" : {
-				blockHeight: 629999, blockHash: "0000000000000000000d656be18bb095db1b23bd797266b0ac3ba720b1962b1e"
-			},
-			"7836d12e741ffc6e50dba9b461e117cfbe444e7daa73df648b3a441d5a9ee958" : {
-				blockHeight: 230009, blockHash: "00000000000000ecbbff6bafb7efa2f7df05b227d5c73dca8f2635af32a2e949"
-			},
-			"29a3efd3ef04f9153d47a990bd7b048a4b2d213daaa5fb8ed670fb85f13bdbcf" : {
-				blockHeight: 153509, blockHash: "00000000000000fb62bbadc0a9dcda556925b2d0c1ad8634253ac2e83ab8382f"
-			},
-			"fe28050b93faea61fa88c4c630f0e1f0a1c24d0082dd0e10d369e13212128f33" : {
-				blockHeight: 1000, blockHash: "00000000c937983704a73af28acdec37b049d214adbda81d7e2a3dd146f6ed09"
-			},
-			"b10c007c60e14f9d087e0291d4d0c7869697c6681d979c6639dbd960792b4d41" : {
-				blockHeight: 692261, blockHash: "0000000000000000000f14c35b2d841e986ab5441de8c585d5ffe55ea1e395ad"
-			},
-			"777c998695de4b7ecec54c058c73b2cab71184cf1655840935cd9388923dc288" : {
-				blockHeight: 709632, blockHash: "0000000000000000000687bca986194dc2c1f949318629b44bb54ec0a94d8244"
-			},
-			"b53e3bc5edbb41b34a963ecf67eb045266cf841cab73a780940ce6845377f141" : {
-				blockHeight: 608548, blockHash: "00000000000000000009cf4a72b39c634586e6e328365f0d7293964111148094"
+			"ae9699ea8afbc5eed576e2b3be16ce69e1e3c9352191cc1bf45a98d7ec2f83fd" : {
+				blockHeight: 721000, blockHash: "198a7b4de1df9478e2463bd99d75b714eab235a2e63e741641dc8a759a9840e5"
 			}
 		}
 	},
-	genesisCoinbaseOutputAddressScripthash:"8b01df4e368ea28f8dc0423bcf7a4923e3a12d307c875e47a0cfbf90b5c39161",
-	historicalData: ltcFun.items,
+	genesisCoinbaseOutputAddressScripthash:"97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9",
 	exchangeRateData:{
 		jsonUrl:"https://api.coindesk.com/v1/bpi/currentprice.json",
 		responseBodySelectorFunction:function(responseBody) {
